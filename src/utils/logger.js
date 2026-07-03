16747d35f38d0e4605d90e935ee7acd48878589a
BinalPatel29 <binaldpatel29@gmail.com>
2026-07-03 16:34:34 +0530

    create logger file

---
+++ src/utils/logger.js
@@ -0,0 +1,10 @@
+import pino from 'pino'
+
+const logger = pino ({
+    level: process.env.LOG_LEVEL || 'info',
+    timestamp: pino.stdTimeFunctions.isoTime,
+    mixin(){
+        return { service: 'note-api-service' };
+    }
+});
+export default logger;
