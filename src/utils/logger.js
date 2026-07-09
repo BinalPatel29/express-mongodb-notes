7d950234329cef5184581e4b0de5a22d6c988a9b
BinalPatel29 <binaldpatel29@gmail.com>
2026-07-09 18:32:34 +0530

    add pino-pretty

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,21 @@
-import pino from 'pino'     // writes super-fast server logs
+import pino from 'pino'; // writes super-fast server logs
 
-const logger = pino ({
+const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
     timestamp: pino.stdTimeFunctions.isoTime,
-    mixin(){
+    mixin() {
         return { service: 'note-api-service' };
+    },
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,          
+            translateTime: 'SYS:yyyy-mm-dd HH:MM:ss', 
+            ignore: '',             
+            singleLine: false,       
+            messageFormat: '{msg}'   
+        }
     }
 });
+
 export default logger;
