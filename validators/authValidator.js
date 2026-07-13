bac78aba06a123a741c53b6512c92fa8c3392b1b
BinalPatel29 <binaldpatel29@gmail.com>
2026-07-13 15:53:48 +0530

    Set abortEarly: false

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -1,42 +1,38 @@
 import joi from 'joi';
 
-export function validateRegister(user){
-    const registerSchema = joi.object ({
-        
-        firstName: joi.string()
-                      .min(3)
-                      .required(),
+export function validateRegister(user) {
+  const registerSchema = joi.object({
+    firstName: joi.string().min(3).required(),
+    lastName: joi.string().min(3).required(),
+    email: joi.string().email().min(5).max(50).required(),
+    password: joi.string().min(6).required(),
+    mobileNo: joi.string().length(10).pattern(/^[0-9]+$/).required(),
+  });
 
-        lastName: joi.string()
-                      .min(3)
-                      .required(),
-
-        email: joi.string()
-                  .email()
-                  .min(5)
-                  .max(50)
-                  .required(),
-        
-        password: joi.string()
-                     .min(6)
-                     .required(),
-                    
-        mobileNo: joi.string()
-                     .length(10)
-                     .pattern(/^[0-9]+$/),
-    });
-    return registerSchema.validate(user);
+  const { error, value } = registerSchema.validate(user, { abortEarly: false });
+  if (error) {
+    const errorMap = error.details.reduce((acc, detail) => {
+      acc[detail.path.join('.')] = detail.message;
+      return acc;
+    }, {});
+    return { error: errorMap, value };
+  }
+  return { error: null, value };
 }
 
-export function validateLogin(user){
-    const loginSchema = joi.object ({
+export function validateLogin(user) {
+  const loginSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required()
+  });
 
-         email: joi.string()
-                   .email()
-                   .required(),
-        
-        password: joi.string()
-                     .required()
-    });
-    return loginSchema.validate(user);
+  const { error, value } = loginSchema.validate(user, { abortEarly: false });
+  if (error) {
+    const errorMap = error.details.reduce((acc, detail) => {
+      acc[detail.path.join('.')] = detail.message;
+      return acc;
+    }, {});
+    return { error: errorMap, value };
+  }
+  return { error: null, value };
 }
