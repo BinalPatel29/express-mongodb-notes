18bf047a559a0f6d27aa8610d0c40b971fa6dc60
BinalPatel29 <binaldpatel29@gmail.com>
2026-07-13 15:48:13 +0530

    add try and catch block

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,21 @@
 import jwt from 'jsonwebtoken';
 
-export function protect(req,res,next){
-    const authHeader = req.headers.authorization;
+export function protect(req, res, next) {
+  const authHeader = req.headers.authorization;
 
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-         const error = new Error("No token provided");
-         error.statusCode = 401;        // unauthorized
-         throw error;
-    }
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    const error = new Error("No token provided");
+    error.statusCode = 401;
+    return next(error);
+  }
 
-    const token = authHeader.split(' ')[1];
+  const token = authHeader.split(' ')[1];
+
+  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
     next();
+  } catch (error) {
+    next(error);
+  }
 }
