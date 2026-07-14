1af69f2abe7237ecea4d0f2008ff12fc6a6368f6
BinalPatel29 <binaldpatel29@gmail.com>
2026-07-14 13:30:24 +0530

    correct host path

--- src/queue/imageQueue.js
+++ src/queue/imageQueue.js
@@ -1,7 +1,7 @@
 import { Queue } from 'bullmq';
 
 const redisOptions = { 
-    host: process.env.REDIS_HOST || "localhost", 
+    host: process.env.REDIS_HOST || "127.0.0.1", 
     port: parseInt(process.env.REDIS_PORT || "6379", 10),
     maxRetriesPerRequest: null 
 };
