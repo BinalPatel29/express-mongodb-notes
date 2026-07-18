202930a9c4b5001875955137a55eac18de6aa648
BinalPatel29 <binaldpatel29@gmail.com>
2026-07-18 16:42:26 +0530

    correct error

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,30 +1,35 @@
-import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken'; 
+import { Request, Response, NextFunction } from 'express'; 
 
-interface CustomRequest extends Request {
-  userId?: string;
-}
+interface CustomRequest extends Request { 
+  userId?: string; 
+} 
 
-interface EnhanceError extends Error {
-  statusCode? : number;
-}
+interface EnhanceError extends Error { 
+  statusCode?: number; 
+} 
 
-export function protect(req: CustomRequest, res: Response, next: NextFunction) {
-  const authHeader = req.headers.authorization;
+export function protect(req: CustomRequest, res: Response, next: NextFunction) { 
+  const authHeader = req.headers.authorization; 
+  if (!authHeader || !authHeader.startsWith('Bearer ')) { 
+    const error: EnhanceError = new Error("No token provided"); 
+    error.statusCode = 401; 
+    return next(error); 
+  } 
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    const error: EnhanceError = new Error("No token provided");
+  const token = authHeader.split(' ')[1]; 
+  
+  if (!token) {
+    const error: EnhanceError = new Error("Malformed token configuration");
     error.statusCode = 401;
     return next(error);
   }
 
-  const token = authHeader.split(' ')[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env['JWT_SECRET'] || '') as jwt.JwtPayload;
-    req.userId = decoded.userId;
-    next();
-  } catch (error) {
-    next(error);
-  }
+  try { 
+    const decoded = jwt.verify(token, process.env['JWT_SECRET'] || '') as jwt.JwtPayload; 
+    req.userId = decoded.userId; 
+    next(); 
+  } catch (error) { 
+    next(error); 
+  } 
 }
