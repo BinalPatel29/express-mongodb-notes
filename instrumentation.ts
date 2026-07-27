595b959c16e440a76ee1efc8571e9f8bcfb9193c
BinalPatel29 <binaldpatel29@gmail.com>
2026-07-27 12:48:53 +0530

    build trace file

---
+++ instrumentation.ts
@@ -0,0 +1,29 @@
+import { register } from "node:module"; register("ts-node/esm", import.meta.url);
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { AlwaysOnSampler } from '@opentelemetry/sdk-trace-base';
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT 
+      ? `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces` 
+      : 'http://localhost:4318/v1/traces', 
+  }),
+  sampler: new AlwaysOnSampler(),  serviceName: 'note-app-pipeline',
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-express': { enabled: true },
+      '@opentelemetry/instrumentation-mongoose': { enabled: true },
+    }),
+  ],
+});
+
+sdk.start();
+
+process.on('SIGTERM', () => {
+  sdk.shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.log('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+});
